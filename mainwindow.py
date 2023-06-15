2aa341ed1231b0ae8b2b29ce1386e944dc7ec01d
Andre Jonas <nipsky@googlemail.com>
2023-06-15 16:13:53 +0200

    Fixed unnoticed bug

--- mainwindow.py
+++ mainwindow.py
@@ -784,7 +784,7 @@ class MainWindow(QtWidgets.QMainWindow):
             return
 
         # Stop the player if it's already playing
-        if self.player_thread.status == PLAYERTHREADSTATUS.PLAYING or PLAYERTHREADSTATUS.PAUSED:
+        if self.player_thread.status == (PLAYERTHREADSTATUS.PLAYING or PLAYERTHREADSTATUS.PAUSED):
             self.stop(False)
 
         # Get song from user data in column
