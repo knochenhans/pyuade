0e328427fa4674b9ad7b657e66a41d9d4143f36a
Andre <nipsky@googlemail.com>
2024-08-11 12:48:52 +0200

    Add cleanup code to stop "uadecore" processes on exit

--- pyuade.py
+++ pyuade.py
@@ -5,11 +5,18 @@ from PySide6 import QtWidgets
 from ctypes_functions import *
 from mainwindow import MainWindow
 from uade import *
+import atexit
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = MainWindow()
     widget.show()
 
     sys.exit(app.exec())
+
+def exit():
+    # Kill remaining "uadecore" processes
+    libuade.uade_stop(None)
+    
+atexit.register(exit)
