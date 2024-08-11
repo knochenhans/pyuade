50791585d1b21e693e642d1165f13e815b18154f
Andre <nipsky@googlemail.com>
2024-08-11 13:14:51 +0200

    Reformatting

--- loader_thread.py
+++ loader_thread.py
@@ -1,25 +1,27 @@
-import os
-
-#import debugpy
-from PySide6.QtCore import Qt, QThread
+import debugpy
+from PySide6.QtCore import Qt, QThread, Signal
 from PySide6.QtWidgets import QProgressDialog
 
 
 class LoaderThread(QThread):
+    finished = Signal()
+
     def __init__(self, parent) -> None:
         super().__init__(parent)
 
         self.filenames: list[str] = []
 
     def run(self):
-        #debugpy.debug_this_thread()
+        # debugpy.debug_this_thread()
 
         from mainwindow import MainWindow
 
         main_window = self.parent()
 
         if isinstance(main_window, MainWindow) and len(self.filenames) > 0:
-            progress = QProgressDialog('Scanning files...', 'Cancel', 0, len(self.filenames), main_window)
+            progress = QProgressDialog(
+                "Scanning files...", "Cancel", 0, len(self.filenames), main_window
+            )
             progress.setWindowModality(Qt.WindowModality.WindowModal)
 
             for i, filename in enumerate(self.filenames):
